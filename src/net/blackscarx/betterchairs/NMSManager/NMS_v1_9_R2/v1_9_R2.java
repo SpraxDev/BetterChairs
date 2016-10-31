199e363ff4adb5e3374310fd7850b032660c18ec
BuildTools <unconfigured@null.spigotmc.org>
2016-10-31 12:40:03 +0100

    Initial commit

---
+++ src/net/blackscarx/betterchairs/NMSManager/NMS_v1_9_R2/v1_9_R2.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright (c) BlackScarx
+ */
+
+package net.blackscarx.betterchairs.NMSManager.NMS_v1_9_R2;
+
+import net.blackscarx.betterchairs.NMS;
+import net.minecraft.server.v1_9_R2.EntityArmorStand;
+import net.minecraft.server.v1_9_R2.IChatBaseComponent;
+import net.minecraft.server.v1_9_R2.PacketPlayOutChat;
+import org.bukkit.Location;
+import org.bukkit.craftbukkit.v1_9_R2.entity.CraftArmorStand;
+import org.bukkit.craftbukkit.v1_9_R2.entity.CraftPlayer;
+import org.bukkit.entity.ArmorStand;
+import org.bukkit.entity.Player;
+
+public class v1_9_R2 implements NMS {
+
+    @Override
+    public String getVersion() {
+        return "v1_9_R2";
+    }
+
+    @Override
+    public ArmorStand spawn(Location loc, Player p) {
+        return CustomArmorStand_9_R2.spawn(loc, p);
+    }
+
+    @Override
+    public void kill(ArmorStand armorStand) {
+        EntityArmorStand nmsArmor = ((CraftArmorStand) armorStand).getHandle();
+        ((CustomArmorStand_9_R2) nmsArmor).killArmorStand();
+    }
+
+    @Override
+    public void sendUpdate(Player p) {
+        IChatBaseComponent msg = IChatBaseComponent.ChatSerializer.a("[\"\",{\"text\":\"[UPDATE]\",\"color\":\"aqua\",\"bold\":true,\"clickEvent\":{\"action\":\"open_url\",\"value\":\"https://www.spigotmc.org/resources/18705/\"},\"hoverEvent\":{\"action\":\"show_text\",\"value\":{\"text\":\"\",\"extra\":[{\"text\":\"Click for go on the plugin page\",\"color\":\"green\"}]}}}]");
+        PacketPlayOutChat packet = new PacketPlayOutChat(msg, (byte) 0);
+        ((CraftPlayer) p).getHandle().playerConnection.sendPacket(packet);
+    }
+
+    @Override
+    public boolean check(ArmorStand armorStand) {
+        return ((CraftArmorStand) armorStand).getHandle() instanceof CustomArmorStand_9_R2;
+    }
+
+}
---
+++ src/net/blackscarx/betterchairs/Panel.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) BlackScarx
+ */
+
+package net.blackscarx.betterchairs;
+
+import javax.swing.*;
+
+/**
+ * Created by BlackScarx on 11/05/2016.
+ * BlackScarx all right reserved
+ */
+public class Panel extends JPanel {
+
+    public Panel() {
+    }
+
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(new Panel(), "This is a plugin.\n" +
+                "Just place it in the plugins folder of your server\n" +
+                "BetterChairs Version: 0.8.8", "BetterChairs", JOptionPane.INFORMATION_MESSAGE);
+        System.exit(0);
+    }
+
+}
