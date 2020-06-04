dc3ea0640b351bc2f94d42c80b189d72dcac8d2d
Christian Koop <developer@sprax2013.de>
2020-06-04 21:50:05 +0200

    Delete some no longer needed files
    While recoding, I keep the original files as a reference as long as needed

--- modules/betterchairs-api/src/main/java/net/blackscarx/betterchairs/TempGlobal.java
+++
@@ -1,8 +0,0 @@
-package net.blackscarx.betterchairs;
-
-import java.util.HashMap;
-import java.util.Map;
-
-public class TempGlobal {
-    public static Map<Integer, ChairsConf> list = new HashMap<>();
-}
--- modules/betterchairs-plugin/src/main/java/net/blackscarx/betterchairs/ChairsPlugin.java
+++ modules/betterchairs-plugin/src/main/java/net/blackscarx/betterchairs/ChairsPlugin.java
@@ -1,10 +1,7 @@
 package net.blackscarx.betterchairs;
 
-import net.blackscarx.betterchairs.Files.Config;
-import net.blackscarx.betterchairs.Files.Messages;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
-import org.bukkit.Location;
 import org.bukkit.block.Block;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -16,7 +13,6 @@ import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.scheduler.BukkitRunnable;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -25,28 +21,17 @@ import java.util.UUID;
 
 public class ChairsPlugin extends JavaPlugin implements Listener {
     public ChairsPlugin plugin;
-    //    public boolean isRegister = false;
     public List<UUID> disableList = new ArrayList<>();
     public List<UUID> uuidList = new ArrayList<>();
 
     @Override
     public void onEnable() {
-        Config.init(this);
-        Messages.init(this);
         Objects.requireNonNull(getCommand("betterchairsreload")).setExecutor(new CmdReload());
         Objects.requireNonNull(getCommand("betterchairstoggle")).setExecutor(new ChairsToggle());
         Objects.requireNonNull(getCommand("betterchairsreset")).setExecutor(new ChairsReset());
         plugin = this;
         PluginManager pm = Bukkit.getPluginManager();
         pm.registerEvents(this, this);
-    }
-
-    @Override
-    public void onDisable() {
-        for (ChairsConf cc : TempGlobal.list.values()) {
-            Player p = cc.getP();
-            p.teleport(cc.getLoc());
-        }
     }
 
     /**
@@ -79,93 +64,38 @@ public class ChairsPlugin extends JavaPlugin implements Listener {
 //            }
 //        }
 
-        //Check if the plugin use permission for sit
-        if (Config.getConfig().getBoolean("Use permission for sit", false)) {
-            if (!p.hasPermission("betterchairs.use")) {
-                if (!uuidList.contains(p.getUniqueId()))
-                    p.sendMessage(ChatColor.translateAlternateColorCodes('&', Messages.getConfig().getString("Cant use message")));
-                uuidList.add(p.getUniqueId());
-                final Player pf = p;
-                new BukkitRunnable() {
-                    @Override
-                    public void run() {
-                        uuidList.remove(pf.getUniqueId());
-                    }
-                }.runTaskLater(this, 2L);
-                return;
-            }
-        }
-        //Check the distance of the chairs
-        if (p.getLocation().distance(b.getLocation().add(0.5, 0, 0.5)) >= Config.getConfig().getDouble("Distance of the stairs", 2.0)) {
-            return;
-        }
-
-        Location loc = b.getLocation().add(0.5, -1.2, 0.5);
+//        Location loc = b.getLocation().add(0.5, -1.2, 0.5);
 
 //        if (!event.isCancelled()) {
         // Check auto turn
-        if (Config.getConfig().getBoolean("AutoTurn", true)) {
-            short data = b.getState().getData().toItemStack().getDurability();
-            switch (data) {
-                case 0:
-                    p.teleport(new Location(p.getWorld(), p.getLocation().getX(), p.getLocation().getY(),
-                            p.getLocation().getZ(), 90f, 0f));
-                    loc.setYaw(90f);
-                    break;
-                case 1:
-                    p.teleport(new Location(p.getWorld(), p.getLocation().getX(), p.getLocation().getY(),
-                            p.getLocation().getZ(), -90f, 0f));
-                    loc.setYaw(-90f);
-                    break;
-                case 2:
-                    p.teleport(new Location(p.getWorld(), p.getLocation().getX(), p.getLocation().getY(),
-                            p.getLocation().getZ(), -180f, 0f));
-                    loc.setYaw(-180f);
-                    break;
-                case 3:
-                    p.teleport(new Location(p.getWorld(), p.getLocation().getX(), p.getLocation().getY(),
-                            p.getLocation().getZ(), 0f, 0f));
-                    loc.setYaw(0f);
-                    break;
-                default:
-                    break;
-            }
-        }
-
-        if (Config.getConfig().getBoolean("Send message when player sit", false)) {
-            if (!uuidList.contains(p.getUniqueId()))
-                p.sendMessage(ChatColor.translateAlternateColorCodes('&', Messages.getConfig()
-                        .getString("Message to send when player sit", "&aYou are now sitting. Take a break.")));
-            uuidList.add(p.getUniqueId());
-            final Player pf = p;
-            new BukkitRunnable() {
-                @Override
-                public void run() {
-                    uuidList.remove(pf.getUniqueId());
-                }
-            }.runTaskLater(this, 2L);
-        }
+//        if (Config.getConfig().getBoolean("AutoTurn", true)) {
+//            short data = b.getState().getData().toItemStack().getDurability();
+//            switch (data) {
+//                case 0:
+//                    p.teleport(new Location(p.getWorld(), p.getLocation().getX(), p.getLocation().getY(),
+//                            p.getLocation().getZ(), 90f, 0f));
+//                    loc.setYaw(90f);
+//                    break;
+//                case 1:
+//                    p.teleport(new Location(p.getWorld(), p.getLocation().getX(), p.getLocation().getY(),
+//                            p.getLocation().getZ(), -90f, 0f));
+//                    loc.setYaw(-90f);
+//                    break;
+//                case 2:
+//                    p.teleport(new Location(p.getWorld(), p.getLocation().getX(), p.getLocation().getY(),
+//                            p.getLocation().getZ(), -180f, 0f));
+//                    loc.setYaw(-180f);
+//                    break;
+//                case 3:
+//                    p.teleport(new Location(p.getWorld(), p.getLocation().getX(), p.getLocation().getY(),
+//                            p.getLocation().getZ(), 0f, 0f));
+//                    loc.setYaw(0f);
+//                    break;
+//                default:
+//                    break;
+//            }
 //        }
-    }
-
-    @SuppressWarnings("unused")
-    private boolean isValidWorld(Player p) {
-        if (Config.getConfig().getStringList("Disable world").contains(p.getWorld().getName())) {
-            if (Config.getConfig().getBoolean("Send message if the word is disable", false)) {
-                if (!uuidList.contains(p.getUniqueId()))
-                    p.sendMessage(ChatColor.translateAlternateColorCodes('&', Messages.getConfig().getString("Message to send if the word is disable", "&cThe Chairs is not enable in this world")));
-                uuidList.add(p.getUniqueId());
-                final Player pf = p;
-                new BukkitRunnable() {
-                    @Override
-                    public void run() {
-                        uuidList.remove(pf.getUniqueId());
-                    }
-                }.runTaskLater(this, 2L);
-            }
-            return false;
-        }
-        return true;
+//        }
     }
 
     /**
@@ -174,9 +104,9 @@ public class ChairsPlugin extends JavaPlugin implements Listener {
     public class CmdReload implements CommandExecutor {
         @Override
         public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
-            Config.reload();
+//            Config.reload();
             reloadConfig();
-            Messages.init(plugin);
+//            Messages.init(plugin);
 //            if (Config.getConfig().getBoolean("Update Checker", true)) {
 //                if (!isRegister) {
 //                    Bukkit.getPluginManager().registerEvents(um, plugin);
@@ -204,10 +134,10 @@ public class ChairsPlugin extends JavaPlugin implements Listener {
             Player p = (Player) commandSender;
             if (disableList.contains(p.getUniqueId())) {
                 disableList.remove(p.getUniqueId());
-                p.sendMessage(ChatColor.translateAlternateColorCodes('&', Messages.getConfig().getString("Message to send when player toggle chairs to on", "&aYou can sit now")));
+//                p.sendMessage(ChatColor.translateAlternateColorCodes('&', Messages.getConfig().getString("Message to send when player toggle chairs to on", "&aYou can sit now")));
             } else {
                 disableList.add(p.getUniqueId());
-                p.sendMessage(ChatColor.translateAlternateColorCodes('&', Messages.getConfig().getString("Message to send when player toggle chairs to off", "&cYou can't sit now")));
+//                p.sendMessage(ChatColor.translateAlternateColorCodes('&', Messages.getConfig().getString("Message to send when player toggle chairs to off", "&cYou can't sit now")));
             }
             return true;
         }
@@ -229,7 +159,7 @@ public class ChairsPlugin extends JavaPlugin implements Listener {
 //                }
 //            }
 
-            TempGlobal.list.clear();
+//            TempGlobal.list.clear();
             sender.sendMessage("§aBetterChairs reset");
             return true;
         }
