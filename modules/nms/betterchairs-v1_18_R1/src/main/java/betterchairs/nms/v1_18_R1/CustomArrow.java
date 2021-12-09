5747ee837619584972589310bf42bd020297bb98
Christian Koop <contact@sprax2013.de>
2021-12-09 07:49:02 +0100

    Add Minecraft 1.18 support

---
+++ modules/nms/betterchairs-v1_18_R1/src/main/java/betterchairs/nms/v1_18_R1/CustomArrow.java
@@ -0,0 +1,64 @@
+package betterchairs.nms.v1_18_R1;
+
+import de.sprax2013.betterchairs.ChairUtils;
+import de.sprax2013.betterchairs.CustomChairEntity;
+import de.tr7zw.changeme.nbtapi.NBTEntity;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.player.EntityHuman;
+import net.minecraft.world.entity.projectile.EntityTippedArrow;
+import net.minecraft.world.level.World;
+
+class CustomArrow extends EntityTippedArrow implements CustomChairEntity {
+    private boolean remove = false;
+    private final int regenerationAmplifier;
+
+    /**
+     * @param regenerationAmplifier provide a negative value to disable regeneration
+     */
+    public CustomArrow(World world, double d0, double d1, double d2, int regenerationAmplifier) {
+        super(world, d0, d1, d2);
+
+        this.regenerationAmplifier = regenerationAmplifier;
+    }
+
+    @Override
+    public void markAsRemoved() {
+        this.remove = true;
+    }
+
+    @Override
+    public void k() {   // tick
+        if (this.remove) return; // If the entity is being removed, no need to bother
+        if (this.S % 10 == 0) return;  // Only run every 10 ticks
+
+        Entity passenger = this.cF().isEmpty() ? null : this.cF().get(0);
+
+        if (!(passenger instanceof EntityHuman)) {
+            remove = true;
+            this.getBukkitEntity().remove();
+            return;
+        }
+
+        // Rotate the entity together with its passenger
+        this.a(this.dm() /* getYRot */, this.dn() /* getXRot */);
+        this.l(this.dm() /* getYRot */); // setHeadRotation
+
+        ChairUtils.applyRegeneration(((EntityHuman) passenger).getBukkitEntity(), this.regenerationAmplifier);
+    }
+
+    @Override
+    public void ag() {
+        // Prevents the ArmorStand from getting killed unexpectedly
+        if (shouldDie()) super.ag();    // killEntity
+    }
+
+    @Override
+    public void a(Entity.RemovalReason removalReason) {
+        // Prevents the ArmorStand from getting killed unexpectedly
+        if (shouldDie()) super.a(removalReason);
+    }
+
+    private boolean shouldDie() {
+        return remove || this.cF().isEmpty() || !(this.cF().get(0) instanceof EntityHuman);
+    }
+}
