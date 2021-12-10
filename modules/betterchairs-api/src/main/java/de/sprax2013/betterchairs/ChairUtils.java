7c719abc8bcfdaced867179b932e2c7aae9aa50d
Christian Koop <contact@sprax2013.de>
2021-12-10 21:57:43 +0100

    Update javadocs for `ChairUtils#applyChairProtections`

--- modules/betterchairs-api/src/main/java/de/sprax2013/betterchairs/ChairUtils.java
+++ modules/betterchairs-api/src/main/java/de/sprax2013/betterchairs/ChairUtils.java
@@ -24,11 +24,14 @@ public class ChairUtils {
     }
 
     /**
+     * Examples for protections that could be applied:
      * <ul>
      *  <li>{@link ArmorStand#hasGravity()} = {@code false}</li>
      *  <li>{@link ArmorStand#isVisible()} = {@code false}</li>
      *  <li>{@link ArmorStand#getRemoveWhenFarAway()} = {@code true}</li>
+     *  <li>{@link Projectile#doesBounce()} = {@code false}</li>
      *  <li>Invulnerable = {@code true}</li>
+     *  <li>NoGravity = {@code true}</li>
      *  <li>Silent = {@code 1}</li>
      *  <li>DisabledSlots = {@code 0b11111} <em>(all)</em></li>
      * </ul>
