abc06aef6714fedc59995ac7d22d69f292b1d1bd
Christian Koop <contact@sprax2013.de>
2024-11-02 16:07:38 +0100

    feat: implement nms module for v1_21_R2 servers

---
+++ modules/nms/betterchairs-v1_21_R2/src/main/java/betterchairs/nms/v1_21_R2/CustomArmorStand.java
@@ -0,0 +1,82 @@
+package betterchairs.nms.v1_21_R2;
+
+import de.sprax2013.betterchairs.ChairManager;
+import de.sprax2013.betterchairs.ChairUtils;
+import de.sprax2013.betterchairs.CustomChairEntity;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.decoration.ArmorStand;
+import net.minecraft.world.entity.player.Player;
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+
+class CustomArmorStand extends ArmorStand implements CustomChairEntity {
+    private boolean remove = false;
+    private final int regenerationAmplifier;
+
+    private final Location expectedLocation;
+
+    /**
+     * @param regenerationAmplifier provide a negative value to disable regeneration
+     */
+    public CustomArmorStand(ServerLevel world, double d0, double d1, double d2, int regenerationAmplifier) {
+        super(world, d0, d1, d2);
+
+        this.regenerationAmplifier = regenerationAmplifier;
+        this.expectedLocation = new Location(null, d0, d1, d2);
+    }
+
+    @Override
+    public void markAsRemoved() {
+        this.remove = true;
+    }
+
+    @Override
+    public void tick() {
+        if (this.remove) return; // If the ArmorStand is being removed, no need to bother
+        if (this.tickCount % 10 == 0) return;  // Only run every 10 ticks
+
+        Entity passenger = getFirstPassenger();
+
+        if (!(passenger instanceof Player)) {
+            this.remove = true;
+            getBukkitEntity().remove();
+            return;
+        }
+
+        // Rotate the ArmorStand together with its passenger
+        // Not happy about using Bukkit API here (+ scheduling) but I don't see a good alternative with all the obfuscation
+        Bukkit.getScheduler().runTask(ChairManager.getPlugin(), () -> getBukkitEntity().setRotation(passenger.getBukkitYaw(), 0));
+
+        if (ChairUtils.didChairEntityMove(this.expectedLocation, getX(), getY(), getZ())) {
+            this.expectedLocation.setY(Math.min(getY(), this.expectedLocation.getY()));
+
+            teleportTo(this.expectedLocation.getX(), this.expectedLocation.getY(), this.expectedLocation.getZ());
+        }
+
+        ChairUtils.applyRegeneration(((Player) passenger).getBukkitEntity(), this.regenerationAmplifier);
+    }
+
+    @Override
+    public void kill(ServerLevel serverLevel) {
+        // Prevents the ArmorStand from getting killed unexpectedly
+        if (shouldDie()) super.kill(serverLevel);
+    }
+
+    @Override
+    public void kill(ServerLevel serverLevel, DamageSource damageSource) {
+        // Prevents the ArmorStand from getting killed unexpectedly
+        if (shouldDie()) super.kill(serverLevel, damageSource);
+    }
+
+    @Override
+    public void remove(Entity.RemovalReason removalReason) {
+        // Prevents the ArmorStand from getting killed unexpectedly
+        if (shouldDie()) super.remove(removalReason);
+    }
+
+    private boolean shouldDie() {
+        return this.remove || getPassengers().isEmpty() || !(getFirstPassenger() instanceof Player);
+    }
+}
---
+++ modules/nms/betterchairs-v1_21_R2/src/main/java/betterchairs/nms/v1_21_R2/v1_21_R2.java
@@ -0,0 +1,98 @@
+package betterchairs.nms.v1_21_R2;
+
+import de.sprax2013.betterchairs.ChairManager;
+import de.sprax2013.betterchairs.ChairNMS;
+import de.sprax2013.betterchairs.ChairUtils;
+import de.sprax2013.betterchairs.CustomChairEntity;
+import de.sprax2013.betterchairs.Messages;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.entity.Entity;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.block.BlockFace;
+import org.bukkit.block.data.Bisected;
+import org.bukkit.block.data.Directional;
+import org.bukkit.block.data.type.Slab;
+import org.bukkit.block.data.type.Stairs;
+import org.bukkit.craftbukkit.v1_21_R2.CraftWorld;
+import org.bukkit.craftbukkit.v1_21_R2.entity.CraftEntity;
+import org.bukkit.entity.Player;
+import org.bukkit.event.entity.CreatureSpawnEvent;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Objects;
+
+@SuppressWarnings("unused")
+public class v1_21_R2 extends ChairNMS {
+    @Override
+    public @NotNull org.bukkit.entity.Entity spawnChairEntity(@NotNull Location loc, int regenerationAmplifier, boolean useArmorStand) {
+        ServerLevel nmsWorld = ((CraftWorld) Objects.requireNonNull(loc.getWorld())).getHandle();
+
+        Entity nmsEntity;
+
+        if (useArmorStand) {
+            nmsEntity = new CustomArmorStand(nmsWorld, loc.getX(), loc.getY() - .25, loc.getZ(), regenerationAmplifier);
+        } else {
+            nmsEntity = new CustomArrow(nmsWorld, loc.getX(), loc.getY() + .3, loc.getZ() + .25, regenerationAmplifier);
+        }
+
+        org.bukkit.entity.Entity bukkitEntity = nmsEntity.getBukkitEntity();
+
+        ChairUtils.applyChairProtections(bukkitEntity);
+
+        if (!nmsWorld.addFreshEntity(nmsEntity, CreatureSpawnEvent.SpawnReason.CUSTOM)) {
+            ChairManager.getLogger().warning(Messages.ERR_ANOTHER_PLUGIN_PREVENTING_SPAWN);
+        }
+
+        return bukkitEntity;
+    }
+
+    @Override
+    public void killChairEntity(@NotNull org.bukkit.entity.Entity entity) {
+        Entity nmsEntity = ((CraftEntity) entity).getHandle();
+
+        if (!(nmsEntity instanceof CustomChairEntity)) {
+            throw new IllegalArgumentException(String.format(Messages.ERR_NOT_CUSTOM_ENTITY, CustomChairEntity.class.getName()));
+        }
+
+        ((CustomChairEntity) nmsEntity).markAsRemoved();
+        entity.remove();
+    }
+
+    @Override
+    public boolean isStair(@NotNull Block block) {
+        return block.getBlockData() instanceof Stairs;
+    }
+
+    @Override
+    public boolean isStairUpsideDown(@NotNull Block block) {
+        return ((Stairs) block.getBlockData()).getHalf() == Bisected.Half.TOP;
+    }
+
+    @Override
+    public @NotNull
+    BlockFace getBlockRotation(@NotNull Block block) {
+        return ((Directional) block.getBlockData()).getFacing();
+    }
+
+    @Override
+    public boolean isSlab(@NotNull Block block) {
+        return block.getBlockData() instanceof Slab && ((Slab) block.getBlockData()).getType() != Slab.Type.DOUBLE;
+    }
+
+    @Override
+    public boolean isSlabTop(@NotNull Block block) {
+        return ((Slab) block.getBlockData()).getType() == Slab.Type.TOP;
+    }
+
+    @Override
+    public boolean hasEmptyMainHand(@NotNull Player player) {
+        return player.getInventory().getItemInMainHand().getType() == Material.AIR;
+    }
+
+    @Override
+    public boolean isChair(@NotNull org.bukkit.entity.Entity entity) {
+        return ((CraftEntity) entity).getHandle() instanceof CustomChairEntity;
+    }
+}
