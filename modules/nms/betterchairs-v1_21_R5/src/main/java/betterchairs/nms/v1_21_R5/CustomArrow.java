3e0ebf42b3761b3351c70534087b4c12bbf4f445
Christian Koop <contact@sprax2013.de>
2025-07-04 10:22:17 +0200

    feat: implement v1_21_R5 nms module for MC 1.21.7/1.21.6 support

---
+++ modules/nms/betterchairs-v1_21_R5/src/main/java/betterchairs/nms/v1_21_R5/CustomArrow.java
@@ -0,0 +1,67 @@
+package betterchairs.nms.v1_21_R5;
+
+import de.sprax2013.betterchairs.ChairManager;
+import de.sprax2013.betterchairs.ChairUtils;
+import de.sprax2013.betterchairs.CustomChairEntity;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.entity.projectile.Arrow;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import org.bukkit.Bukkit;
+
+class CustomArrow extends Arrow implements CustomChairEntity {
+    private boolean remove = false;
+    private final int regenerationAmplifier;
+
+    /**
+     * @param regenerationAmplifier provide a negative value to disable regeneration
+     */
+    public CustomArrow(ServerLevel world, double d0, double d1, double d2, int regenerationAmplifier) {
+        super(world, d0, d1, d2, new ItemStack(Items.ARROW), null);
+
+        this.regenerationAmplifier = regenerationAmplifier;
+    }
+
+    @Override
+    public void markAsRemoved() {
+        this.remove = true;
+    }
+
+    @Override
+    public void tick() {
+        if (this.remove) return; // If the entity is being removed, no need to bother
+        if (this.tickCount % 10 == 0) return;  // Only run every 10 ticks
+
+        Entity passenger = getFirstPassenger();
+
+        if (!(passenger instanceof Player)) {
+            this.remove = true;
+            getBukkitEntity().remove();
+            return;
+        }
+
+        // Rotate the entity together with its passenger
+        // Not happy about using Bukkit API here (+ scheduling) but I don't see a good alternative with all the obfuscation
+        Bukkit.getScheduler().runTask(ChairManager.getPlugin(), () -> getBukkitEntity().setRotation(passenger.getBukkitYaw(), 0));
+
+        ChairUtils.applyRegeneration(((Player) passenger).getBukkitEntity(), this.regenerationAmplifier);
+    }
+
+    @Override
+    public void kill(ServerLevel serverLevel) {
+        // Prevents the ArmorStand from getting killed unexpectedly
+        if (shouldDie()) super.kill(serverLevel);
+    }
+
+    @Override
+    public void remove(Entity.RemovalReason removalReason) {
+        // Prevents the ArmorStand from getting killed unexpectedly
+        if (shouldDie()) super.remove(removalReason);
+    }
+
+    private boolean shouldDie() {
+        return this.remove || getPassengers().isEmpty() || !(getFirstPassenger() instanceof Player);
+    }
+}
